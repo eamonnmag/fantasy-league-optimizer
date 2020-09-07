0033586a322f983207e4da5488c4d94e73d525d0
emaguire <eamonnmag@protonmail.ch>
2020-09-07 21:27:25 +0200

    more players added

--- assets/js/fantasy-solve.js
+++ assets/js/fantasy-solve.js
@@ -2,15 +2,20 @@ var fantasySolver = (function () {
 
     var processed_data, constraints;
 
+    var DEF_COUNT = 5, MID_COUNT = 5, FWD_COUNT = 3;
+
     function generate_constraints(processed_data) {
 
         var constraints =  {
             "cost": { "max": 100 },
             "GKP": { "max": 1, "min": 1 },
             "GKP_LOW": {"min":1, "max": 1},
-            "DEF": { "max": 5, "min": 5 },
-            "MID": { "max": 5, "min": 5 },
-            "FWD": { "max": 3, "min": 3 }
+            "DEF": { "max": DEF_COUNT, "min": DEF_COUNT },
+            "DEF_LOW": {"min":0, "max": 0},
+            "MID": { "max": MID_COUNT, "min": MID_COUNT},
+            "MID_LOW": {"min":0, "max": 0},
+            "FWD": { "max": FWD_COUNT, "min": FWD_COUNT },
+            "FWD_LOW": { "max": 0, "min": 0 }
         }
 
         processed_data.teams.forEach(function(t) {
@@ -30,8 +35,8 @@ var fantasySolver = (function () {
         Object.keys(players).forEach(function(p) {
             var player = players[p];
             player[p] = 1;
-            if(player.position === "GKP" & +player.cost <= 4) {
-                player["GKP_LOW"] = 1;
+            if(+player.cost <= 4.5) {
+                player[player['position'] + "_LOW"] = 1;
             } else {
                 player[player['position']] = 1;
             }
@@ -64,14 +69,12 @@ var fantasySolver = (function () {
 
     return {
         solve: function (players) {
-            
             processed_data = process_players(players);
             constraints = generate_constraints(processed_data)
-
             return run_solver(constraints, processed_data);
         },
 
-        runWithInitialPlayers: function(positive, negative) {
+        runWithInitialPlayers: function(positive, negative, formation) {
             let _updated = Object.assign({}, constraints);
             positive.forEach(function(d) {
                 _updated[d] = {'min': 1, "max":1};
@@ -80,6 +83,23 @@ var fantasySolver = (function () {
             negative.forEach(function(d) {
                 _updated[d] = {'min': 0, "max":0};
             });
+
+            if(formation !== "all") {
+                var def = +formation[0];
+                var mid = +formation[1];
+                var fwd = +formation[2];
+
+                _updated['DEF'] = {'min': def, 'max': def};
+                _updated['DEF_LOW'] = {'min': DEF_COUNT-def, 'max': DEF_COUNT-def};
+
+                _updated['MID'] = {'min': mid, 'max': mid};
+                _updated['MID_LOW'] = {'min': MID_COUNT-mid, 'max': MID_COUNT-mid};
+
+                _updated['FWD'] = {'min': fwd, 'max': fwd};
+                _updated['FWD_LOW'] = {'min': FWD_COUNT-fwd, 'max': FWD_COUNT-fwd};
+
+                console.log(_updated);
+            }
 
             return run_solver(_updated, processed_data);
         } 
